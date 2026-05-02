253e2a7e364b5930105fc64d5857638f8d82c8a7
creater6753 <duartebaptistaroda@gmail.com>
2026-05-02 17:32:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 function check() {
   let v = document.getElementById("input").value.toLowerCase();
 
-  if (v.includes("movimento") || v.includes("continuo") || v === "going") {
+  if (
+    v.includes("shift") ||
+    v.includes("alfabeto") ||
+    v.includes("cifra") ||
+    v.includes("deslocamento")
+  ) {
     window.location.href = "node7.html";
   } else {
     document.getElementById("msg").innerText =
-      "padrão não reconhecido pelo sistema";
+      "padrão não reconhecido";
   }
 }
